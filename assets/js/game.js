43eac04c1302fe92a09ef3d36a22caf2bd9a36d7
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-09 15:16:21 -0600

    played around with basic js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+var playerName = window.prompt("What is you robot's name?");
+
+console.log(playerName);
