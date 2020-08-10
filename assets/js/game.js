af15ba1bea3844174f7cebb15a438fd5379bc74e
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-08-10 11:42:39 -0600

    added a for loop to go through all of the enemies.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,23 @@
+//Game States
+//"WIN" - Player robot has defeated all enemy robots
+//      * fight all enemy robots
+//      * Defeat each enemy robot
+// "LOSE" - player robot's health is zero or less
+
+//global variables
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//functions
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert users that they are starting the round
-    window.alert("welcome to Robot Gladiators!");
-
     //checking if the user wants to fight
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     console.log(promptFight);
@@ -76,7 +79,10 @@ var fight = function() {
         window.alert("You need to pick a valid option. Try again!");
         fight();
     }
-
 };
 
-fight();
+for (i = 0; i < enemyNames.length; i++) {
+    var currentRound = i + 1;
+    window.alert("welcome to Robot Gladiators! Round " + currentRound); 
+    fight(enemyNames[i]);
+}
